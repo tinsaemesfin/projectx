4330b3de6acdd00e74ff8b7b19e56a12b952effd
Arada_Tech <qmesfin@gmail.com>
2020-05-15 19:45:19 -0700

    registration and llogin from tutorial only

--- app/src/main/java/com/example/project_x/MainActivity.java
+++ app/src/main/java/com/example/project_x/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.project_x;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();//logout
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
